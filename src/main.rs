60a137ad36e22ad59be678d63471b1ea29a306f1
BuildTools <unconfigured@null.spigotmc.org>
2024-06-10 13:33:30 +0100

    Me learning rust lmao to be added on later.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("How much is your airplane ticket?");
     let ticket_price = read_i32() as f64;
     let tax = 0.2; // 20% Percent
-    let final_price = ticket_price * (1.0 + tax);
+    let final_price = ticket_price * (1.0 + tax); //For correct tax calculations
     println!("£{:.2} is the price of your airplane ticket!", final_price);
 }
 
