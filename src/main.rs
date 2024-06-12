99e5b103ede53fc8927d3e2278fc7dc675ad49e7
BuildTools <unconfigured@null.spigotmc.org>
2024-06-12 21:08:36 +0100

    Added taxi.rs Made read_f64 have error handling and read_64 is global function.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::io;
 
+mod airplane;
+mod taxi;
+
 fn main() {
-    println!("How much is your airplane ticket?");
-    let ticket_price = read_f64();
-    let tax = 1.2; // 20% Percent
-    let final_price = ticket_price * tax; //For correct tax calculations
-    println!("£{:.2} is the price of your airplane ticket!", final_price);
+    taxi::run();
+    airplane::run();
 }
 
-fn read_f64() -> f64 {
-    let line = io::stdin().lines().next().unwrap();
-    line.parse().unwrap()
+pub fn read_f64() -> Result<f64,anyhow::Error> {
+    let line = io::stdin().lines().next().expect("Could not get the next line.");
+    Ok(line?.parse::<f64>().expect("Couldn't convert line to f64"))
 }
